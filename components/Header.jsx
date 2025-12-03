96f0c8fce6947624e3c26d2616e55ba2cf1c6868
Raifa509 <fathimathulraifanp@gmail.com>
2025-12-03 19:15:27 +0400

    authentication completed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+'use client'; // MUST be at the top
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { Button } from './ui/button';
+import { ChevronDown, File, FileText, FileTextIcon, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from './ui/dropdown-menu';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-background/60'>
+      <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+        <Link href='/'>
+          <Image src='/logo.png' alt='logo' width={180} height={1} className='  object-contain w-auto' />
+        </Link>
 
-export default Header
+        <div className='flex items-center space-x-2 md:space-x-4'>
+          <SignedIn>
+            <Link href={'/dashboard'}>
+              <Button variant='outline'>
+                <LayoutDashboard className='h-4 w-4' />
+                <span className='hidden md:block'>Industry Insights</span></Button>
+            </Link>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button>
+                  <StarsIcon className='h-4 w-4' />
+                  <span className='hidden md:block'>Growth Tools</span>
+                  <ChevronDown className='h-4 w-4' />
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+
+                  <Link href={'/resume'} className='flex items-center gap-2'>
+                    <FileTextIcon className='h-4 w-4' />
+                    <span>Build Resume</span>
+                  </Link>
+
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <Link href={'/cover-letter'} className='flex items-center gap-2'>
+                    <PenBox className='h-4 w-4' />
+                    <span>Cover Letter</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={'/interview'} className='flex items-center gap-2'>
+                    <GraduationCap className='h-4 w-4' />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>
+              <Button variant='outline'>Sign in</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton appearance={
+              {
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard
+                    : 'shadow-xl',
+                  userPreviewMainIdentifier: "font-semibold"
+                }
+              }
+            } 
+            afterSignOutUrl='/'/>
+          </SignedIn>
+        </div>
+
+
+
+      </nav>
+
+
+    </header>
+  );
+};
+
+export default Header;
