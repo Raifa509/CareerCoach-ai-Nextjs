0dde50ecf4464b7d42cdff7125fc0d9dcc836c50
Raifa509 <fathimathulraifanp@gmail.com>
2025-12-10 16:49:41 +0400

    landing page done

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="text-5xl">Heyy ,its me raifa
-  <br/>
-  <Button>Helloo</Button>
+  <div>
+    <div className="grid-background"></div>
+    <HeroSection/>
   </div>
   
   );
